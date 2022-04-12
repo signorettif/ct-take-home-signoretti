776e1559a28853766632f80e885ba5f7cacf7e95
Francesco Signoretti <signorettif@users.noreply.github.com>
2022-04-12 23:17:47 +0200

    Progress on top search details bar

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,16 +5,23 @@ import Header from '../../components/layout/Header';
 import ContentContainer from '../../components/layout/ContentContainer';
 import SearchDetails from './components/SearchDetails';
 
+const cars = require('../../constants/cars.json')[0]['VehAvailRSCore'];
+
 const HomePage: React.FC = () => {
   // //TODO@ load cars from endpoint using react query
-  // const cars = require('../constants/cars.json');
-  // console.log(cars);
+  console.log(cars);
+
+  const querySearchDetails = cars['VehRentalCore'];
 
   return (
     <Box w="100%">
       <Header />
       <ContentContainer marginTop={16}>
-        <SearchDetails />
+        <SearchDetails
+          PickUpDateTime={querySearchDetails['@PickUpDateTime']}
+          ReturnDateTime={querySearchDetails['@ReturnDateTime']}
+          {...querySearchDetails}
+        />
       </ContentContainer>
     </Box>
   );
--- src/pages/HomePage/components/SearchDetails.tsx
+++ src/pages/HomePage/components/SearchDetails.tsx
@@ -1,44 +1,43 @@
 import React from 'react';
 
-import { Box, Button, Heading, Spacer, Text } from '@chakra-ui/react';
+import { Box, Button, Spacer } from '@chakra-ui/react';
 import { ChevronRightIcon } from '@chakra-ui/icons';
+import SearchLocationDateTime from './SearchLocation';
 
+interface PickUpOrReturnLocation {
+  '@Name': string;
+}
 interface SearchDetailsProps {
-  fromLocation: string;
-  toLocation: string;
-  fromDate: string;
-  toDate: string;
+  PickUpLocation: PickUpOrReturnLocation;
+  ReturnLocation: PickUpOrReturnLocation;
+  PickUpDateTime: string;
+  ReturnDateTime: string;
 }
 
-const SearchDetails: React.FC = () => {
-  return (
-    <Box
-      w="100%"
-      p={4}
-      display="flex"
-      borderRadius={5}
-      backgroundColor="gray.100"
-      alignItems="center"
-    >
-      <Box>
-        <Heading as="h3" size="md">
-          Las Vegas
-        </Heading>
-        <Text>Las Vegas</Text>
-      </Box>
-      <ChevronRightIcon w={12} h={12} marginX={4} />
-      <Box>
-        <Heading as="h3" size="md">
-          Las Vegas
-        </Heading>
-        <Text>Las Vegas</Text>
-      </Box>
-      <Spacer />
-      <Button size="lg" disabled colorScheme="teal">
-        Edit
-      </Button>
-    </Box>
-  );
-};
+const SearchDetails: React.FC<SearchDetailsProps> = ({
+  PickUpLocation,
+  ReturnLocation,
+  PickUpDateTime,
+  ReturnDateTime,
+}) => (
+  <Box
+    w="100%"
+    p={4}
+    display="flex"
+    borderRadius={5}
+    backgroundColor="gray.100"
+    alignItems="center"
+  >
+    <SearchLocationDateTime loc={PickUpLocation} dateTime={PickUpDateTime} />
+    <ChevronRightIcon w={12} h={12} marginX={4} />
+    <SearchLocationDateTime loc={ReturnLocation} dateTime={ReturnDateTime} />
+
+    <Spacer />
+
+    <Button size="lg" disabled colorScheme="teal">
+      Edit
+    </Button>
+  </Box>
+);
 
 export default SearchDetails;
