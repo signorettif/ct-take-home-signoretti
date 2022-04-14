05bb86785cfaf0d89d946a9e85ce2dc434ac06bd
Lorenzo Signoretti <lorenzosigno@gmail.com>
2022-04-14 18:37:26 +0200

    Remove unused `useEffect`

--- src/pages/CarPage/CarPage.tsx
+++ src/pages/CarPage/CarPage.tsx
@@ -1,11 +1,11 @@
-import { Box, Text } from '@chakra-ui/react';
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import CarCard from '../../components/car/CarCard';
-import ContentContainer from '../../components/layout/ContentContainer';
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+import { useParams } from "react-router-dom";
+import CarCard from "../../components/car/CarCard";
+import ContentContainer from "../../components/layout/ContentContainer";
 
-import PageLayout from '../../components/layout/PageLayout';
-import useCar from '../../hooks/useCar';
+import PageLayout from "../../components/layout/PageLayout";
+import useCar from "../../hooks/useCar";
 
 interface CarPageProps {}
 
